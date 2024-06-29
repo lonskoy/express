fa2b296dac17b57dd62d7b9ccb5c507cd5a4de3d
lonskoy <lonskoy0304@gmail.com>
2024-06-29 15:30:34 +0300

    edit2

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 // Подключение API REST
 import { newBook } from "./api/newBook.js";    
@@ -10,22 +13,26 @@ import { allBooks } from "./api/allBooks.js";
 import { findBook } from "./api/findBook.js";
 import { deleteBook } from "./api/deleteBook.js";
 import { updateBook } from "./api/updateBook.js";
-import { downloadBook } from "./api/downloadBook.js";
+
 
 // Подключение Middleware
 import { uploadFile } from "./middleware/uploadFile.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', router)
+app.use('/api', router);
+app.use(express.static(__dirname));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const port = 3000;
 const login = { id: 1, mail: "test@mail.ru" };
 
-router.post("/api/user/login", async (req, res) => {
+router.post("/user/login", async (req, res) => {
   // авторизация пользователя
   res.status(200).json(login);
 });
@@ -36,7 +43,7 @@ router.get("/books", allBooks); // получить все книги
 
 router.get("/books/:id", findBook); // найти книгу по id
 
-router.get("/books/:id/download", downloadBook); // найти книгу по id и скачать ее
+// router.get("/books/download/:id", downloadBook); // найти книгу по id и скачать ее
 
 router.put('/books/:id', updateBook); // редактирование книги
  
--- backend/middleware/uploadFile.js
+++ backend/middleware/uploadFile.js
@@ -1,9 +1,10 @@
 import multer from "multer";
+import path from "path";
 
 // Определяем хранилище для multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Директория для сохранения файлов
+    cb(null, path.join(__dirname, '../uploads')); // Директория для сохранения файлов
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
@@ -11,16 +12,17 @@ const storage = multer.diskStorage({
 });
 
 // Создаем экземпляр multer с заданным хранилищем
-const upload = multer({ storage: storage });
+const uploadMulter = multer({storage});
 
 // Middleware для загрузки файла
 export function uploadFile(req, res, next) {
-  upload.single('file')(req, res, function (err) {
+  uploadMulter.single(req.fileBook)(req, res, function (err) {
     if (err) {
       // Обработка ошибок при загрузке
       return res.status(500).send('Произошла ошибка при загрузке файла.');
     }
     // Продолжаем выполнение следующего middleware или маршрута
+    console.log(`Добавлен файл ${req.fileBook}`)
     next();
   });
 }
